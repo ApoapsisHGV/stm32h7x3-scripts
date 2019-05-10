e87cd7a4ae2cf703618feaaeef1fba90a6061b8a
hargoniX <hargonix@gmail.com>
2019-05-10 22:24:44 +0200

    adding a script to automate the compilation and isntallation of a development version of openocd as the current release doesnt support the stm32h7

---
+++ openocd-setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if hash openocd 2>/dev/null; then
+    echo "openocd already appears to be installed, consider removing it if you want this script to complete"
+    openocd --version
+else
+    echo "OpenOCD doesn't appear to be installed, initializing installation..."
+    echo "Downloading source..."
+    git clone https://repo.or.cz/openocd.git
+    echo "Done"
+    cd openocd
+    echo "Preparing compilation..."
+    ./bootstrap
+    ./configure --disable-werror
+    echo "Done"
+    echo "Compiling..."
+    make
+    echo "Done"
+    echo "Installing..."
+    sudo make install
+fi
